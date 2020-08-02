dc3f824847714be0bb9aa1923c355a9e473600d7
Shalamov Egor <eggorke@gmail.com>
2020-08-02 23:26:28 +0500

    Add navbar and routes

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,13 @@
 class IndexController < ApplicationController
   def home
   end
+
+  def about
+
+  end
+
+  def login
+  
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'signup' => 'users#new'
+  get 'about'  => 'index#about'
+  get 'login'  => 'index#login'
   root 'index#home'
   
 end
