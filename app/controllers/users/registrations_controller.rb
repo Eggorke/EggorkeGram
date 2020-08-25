e97c3442c63fb432b9b81776a1521ae7357ff60e
Shalamov Egor <eggorke@gmail.com>
2020-08-26 00:01:01 +0500

    improve avatar

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
-before_action :configure_account_update_params, only: [:update]
+  # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -14,15 +14,15 @@ before_action :configure_account_update_params, only: [:update]
   #   super
   # end
 
-GET /resource/edit
-def edit
-  super
-end
+  # GET /resource/edit
+  # def edit
+  #   super
+  # end
 
-PUT /resource
-def update
-  super
-end
+  # PUT /resource
+  # def update
+  #   super
+  # end
 
   # DELETE /resource
   # def destroy
@@ -38,7 +38,7 @@ end
   #   super
   # end
 
- protected
+  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
