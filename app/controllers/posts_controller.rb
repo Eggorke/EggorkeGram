d434f608d401161b542cd7b2bad737bba59e9152
Shalamov Egor <eggorke@gmail.com>
2020-08-31 10:05:47 +0500

    add description to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -53,7 +53,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :image)
+    params.require(:post).permit(:title, :image, :comment)
   end
 
   
