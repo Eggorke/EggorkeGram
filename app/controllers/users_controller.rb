f4638813ca7cb40f2ac3a47217ef03cea1a8de7f
Shalamov Egor <eggorke@gmail.com>
2020-09-03 20:04:05 +0500

    add little bit design again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,9 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         @posts = Post.where(user_id: @user.id)
+        
+        if @user.id == current_user.id
+            redirect_to root_path
+        end
     end
 end
