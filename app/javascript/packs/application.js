c599c64a3d12c51819f627550efef04d742f05dd
Shalamov Egor <eggorke@gmail.com>
2020-09-04 20:03:30 +0500

    added js function to hide alert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,18 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+document.addEventListener('DOMContentLoaded', function(){
+
+    function HideAlert(){
+        var x = document.getElementsByClassName('alert')[0];
+        x.classList.add("hide");
+    }
+
+    setTimeout(HideAlert, 10000);
+    
+});
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
