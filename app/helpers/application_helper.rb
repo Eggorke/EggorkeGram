a4098d96bd4a6daa9a2da3dc4662c9b3200852da
Shalamov Egor <eggorke@gmail.com>
2020-09-27 21:40:20 +0500

    Fixed random users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,13 @@
 
 module ApplicationHelper
   def ten_random_users
-    offset = User.order('RANDOM()').limit(11)
-    offset
+    ignoring_ids = []
+    ignoring_ids.push(current_user.id)
+    @preset = current_user.following_users
+    @preset.each do |u|
+      ignoring_ids.push(u.id)
+    end
+    offset = User.where.not(id: ignoring_ids).order('RANDOM()').limit(11)
+    return offset
   end
 end
